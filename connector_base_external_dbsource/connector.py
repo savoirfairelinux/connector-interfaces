5f91002bbf72d44f74f668b1c35ff552dc0388d7
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2015-09-24 11:06:54 -0400

    [IMP] Remove deprecated class

--- connector_base_external_dbsource/connector.py
+++ connector_base_external_dbsource/connector.py
@@ -20,13 +20,13 @@
 #
 ##############################################################################
 
-from openerp.addons.connector.connector import Environment
+from openerp.addons.connector.connector import ConnectorEnvironment
 
 
 def get_environment(session, model_name, backend_id):
     """ Create an environment to work with. """
     backend_record = session.env['dbsource.backend'].browse(backend_id)
-    env = Environment(backend_record, session, model_name)
+    env = ConnectorEnvironment(backend_record, session, model_name)
     lang = backend_record.default_lang_id
     lang_code = lang.code if lang else 'en_US'
     if lang_code == session.context.get('lang'):
